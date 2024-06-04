a946b5abd3421b083303050d40d504f58e4e74b1
Abdurrahman Murat Aktürk <muratakturk97@hotmail.com>
2024-06-04 19:07:57 +0300

    homework5

--- src/WEEK5_TEKNIK_ICERIK_5/HOMEWORK_4.sql
+++ src/WEEK5_TEKNIK_ICERIK_5/HOMEWORK_4.sql
@@ -1,5 +1,5 @@
 --odev4
---1 film tablosunda bulunan replacement_cost sütununda bulunan birbirinden farklı değerleri sıralayınız.
+--1 gitgfilm tablosunda bulunan replacement_cost sütununda bulunan birbirinden farklı değerleri sıralayınız.
 SELECT DISTINCT replacement_cost FROM film;
 --2 film tablosunda bulunan replacement_cost sütununda birbirinden farklı kaç tane veri vardır?
 SELECT COUNT(DISTINCT replacement_cost) FROM film;
